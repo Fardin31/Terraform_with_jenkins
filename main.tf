d1c86388759bfcd56e6e609451e01ea3518db24e
Fardin31 <fardinpathan886@gmail.com>
2024-03-19 13:41:46 +0530

    jenkinsfile has been added

--- main.tf
+++ main.tf
@@ -1,19 +1,21 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.40.0"
     }
   }
 }
 
 provider "aws" {
-  # Configuration options
+
+  region = "us-east-1"
+  
 }
 
 resource "aws_instance" "this_ec2" {
-    ami = "ami-0d7a109bf30624c99"
-    instance_type = "t2.micro"
-    key_name = "fardin12"
-  
+  ami           = "ami-0d7a109bf30624c99"
+  instance_type = "t2.micro"
+  key_name      = "fardin12"
+
 }
